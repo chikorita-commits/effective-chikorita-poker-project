0acfb3e6423757e491ebffadd08362f2c0d189bd
chikorita-commits <61445279+chikorita-commits@users.noreply.github.com>
2020-02-27 14:55:46 -0800

    brb
    the game is over

--- game.py
+++ game.py
@@ -25,11 +25,34 @@ WINNING_HANDS = [ "Royal Flush", \
 # make a PokerCard Class inherit from Card
 class PokerCard(Card):
 
-# make a PokerHand Class
-        
+class PokerHand:
+  
 # make a PokerGame function
 def PokerGame():
-        
+    print("This Video Poker project is still in development. Please report bugs")  
+    Name = input("what is your name?")
+    if Name == "Chikorita":
+      print("Greetings developer.")
+    else:
+      print("Greetings tester.")
+    Age = input("HOW OLD ARE YOU? PLEASE BE HONEST.")
+    if Age <= 21:
+      return "We do not sell credits to players under 21."
+    else:
+      print("We are proceeding with the game")
+      Eula = input("Warning: The game you are playing is very addictive. We are not responsible for any credit losses during your session. Understand?"
+             if Eula == "Yes":
+                   print("Enough talk. Let's start.")
+                   print("How many credits do you have?")
+                   if Credits < 1:
+                     return "You ran out of credits. Please call your local credit facillity."
+                   else:
+                     print("Let's play!")
+                     print("Credits: "+str(Credits))
+                     print(PokerHand)
+             else:
+                   return "Error: User rejected agreement"
+    
     # make the player
     #player = PokerPlayer("Player", 1)
     
