819f9b39bd44faa0bce4a4d6e4f4c6252e664259
chikorita-commits <61445279+chikorita-commits@users.noreply.github.com>
2020-02-27 14:38:54 -0800

    add
    lobster

---
+++ game.py
@@ -0,0 +1,64 @@
+'''
+Created on Feb 6, 2019
+
+@author: Charles Tao, Tyler
+
+Description: Version 0.05: Added more features
+
+'''
+from card import Card
+from player import Player
+from stack_of_cards import StackOfCards
+
+
+# These are the winning hands in order of strength
+WINNING_HANDS = [ "Royal Flush", \
+                  "Straight Flush", \
+                  "Four of a Kind", \
+                  "Full House", \
+                  "Flush", \
+                  "Straight", \
+                  "3 of a Kind", \
+                  "Two Pairs", \
+                  "Pair (Jacks or better)" ]
+
+# make a PokerCard Class inherit from Card
+class PokerCard(Card):
+
+# make a PokerHand Class
+        
+# make a PokerGame function
+def PokerGame():
+        
+    # make the player
+    #player = PokerPlayer("Player", 1)
+    
+    # make a deck of card
+    # deck = PokerHand()  # make empty deck
+    # add the 52-cards and shuffle
+    
+    # make rest of the game
+    
+    #Snapshot 1:
+    # Charles and Tyler
+    # This is me adding a feature to the game.
+    print("You won!")
+# add any other helper functions to organize your code nicely
+    
+def main():
+    PokerGame()
+    
+if __name__ == "__main__":
+    main()
+    
+        
+
+
+
+
+
+
+
+
+
+
